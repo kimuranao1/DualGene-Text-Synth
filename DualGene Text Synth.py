dfd2aca8b0a7aa620c19692295f42d6150677a98
kimuranao1 <129270475+kimuranao1@users.noreply.github.com>
2025-12-27 18:51:57 +0900

    Update DualGene Text Synth.py

--- DualGene Text Synth.py
+++ DualGene Text Synth.py
@@ -1,3 +1,18 @@
+# Copyright 2025 <kmrno>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 # ============================================================
 # Genetic Markov Generator
 # Periodic Embedding + Simulation Selection
@@ -242,5 +257,6 @@ if __name__ == "__main__":
     theme_gene_set = seed_theme_genes(vocab, ivocab, theme_tokens, genes)
 
     print(generate(chain, genes, ivocab, phases, theme_gene_set))
+
 
 
